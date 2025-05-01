75b8c52c271f28d50e4e0c90b975795b3164d9d2
Ljw <ljw3436498954@qq.com>
2025-05-02 07:13:10 +0800

    完成work3

---
+++ jdbc-demo/src/west_2/Sort.java
@@ -0,0 +1,72 @@
+package west_2;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Sort {
+    public static ArrayList<Product> sortProduct(String Item, String function) throws SQLException, ClassNotFoundException {
+        ArrayList<Product> list=new ArrayList<>();
+        Connection conn= GetConn.getConnection();
+        String sql="select Product_id,Product_Name,Product_Stock,Product_price from product order by Product_id asc";
+        if(function.equals("desc")||function.equals("asc")){
+            if(Item.equals("Product_id")||Item.equals("Product_price")){
+                sql="select Product_id,Product_Name,Product_Stock,Product_price from product order by "+Item+" "+function;
+                PreparedStatement pstmt=conn.prepareStatement(sql);
+                try(ResultSet rs=pstmt.executeQuery()){
+                    conn.setAutoCommit(false);
+                    int id=rs.getInt("Product_id");
+                    String name=rs.getString("Product_Name");
+                    int stock=rs.getInt("Product_Stock");
+                    double price=rs.getDouble("Product_price");
+                    Product item=new Product();
+                    item.setProduct_id(id+"");
+                    item.setProduct_name(name);
+                    item.setProduct_Stock(stock+"");
+                    item.setProduct_price(price+"");
+                    list.add(item);
+                }
+                catch (Exception e){
+                    conn.rollback();
+                    throw new MyfunctionException();
+                }
+                finally {
+                    pstmt.close();
+                    conn.close();
+                }
+            }
+        }
+        return list;
+    }
+    public static ArrayList<Order> sortOrder(String Item, String function) throws SQLException, ClassNotFoundException {
+        ArrayList<Order> list=new ArrayList<>();
+        Connection conn= GetConn.getConnection();
+        String sql="select Order_id,Order_time,Order_status from orders order by Order_id asc";
+        if(function.equals("desc")||function.equals("asc")){
+            if(Item.equals("Order_id")||Item.equals("Order_date")){
+                sql="select Order_id,Order_time,Order_status from orders order by "+Item+" "+function;
+                PreparedStatement pstmt=conn.prepareStatement(sql);
+                try(ResultSet rs=pstmt.executeQuery()){
+                    conn.setAutoCommit(false);
+                    int id=rs.getInt("Order_id");
+                    String date=rs.getString("Order_date");
+                    String price=rs.getString("Order_price");
+                    Order item=new Order();
+                    item.setOrder_id(id+"");
+                    item.setOrder_date(date);
+                    item.setOrder_price(price);
+                    list.add(item);
+                } catch (Exception e){
+                    conn.rollback();
+                    throw new MyfunctionException();
+                } finally {
+                    pstmt.close();
+                    conn.close();
+                }
+            }
+        }
+        return list;
+    }
+}
---
+++ jdbc-demo/src/west_2/SortTest.java
@@ -0,0 +1,49 @@
+package west_2;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class SortTest {
+    @Test
+    public void test(){
+        //根据订单编号
+        try {
+            ArrayList<Order> list=Sort.sortOrder("Order_id","asc");
+            for (Order o:list) {
+                System.out.println(o);
+            }
+        }
+        catch (Exception e){
+            System.out.println("排序失败");
+        }
+        //根据订单日期
+        try {
+            ArrayList<Order> list=Sort.sortOrder("Order_date","asc");
+            for (Order o:list) {
+                System.out.println(o);
+            }
+        } catch (Exception e){
+            System.out.println("排序失败");
+        }
+        //根据商品价格
+        try {
+            ArrayList<Product> list=Sort.sortProduct("Product_price","asc");
+            for (Product p:list) {
+                System.out.println(p);
+            }
+        }catch (Exception e){
+            System.out.println("排序失败");
+        }
+        //根据商品编号
+        try {
+            ArrayList<Product> list=Sort.sortProduct("Product_id","asc");
+            for (Product p:list) {
+                System.out.println(p);
+            }
+        } catch (Exception e){
+            System.out.println("排序失败");
+        }
+
+    }
+}
