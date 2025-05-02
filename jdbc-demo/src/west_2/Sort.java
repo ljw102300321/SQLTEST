12524a2bacff58810bbdb3ccbfb2eff861fde82a
Ljw <ljw3436498954@qq.com>
2025-05-02 23:35:54 +0800

    完成work3

--- jdbc-demo/src/west_2/Sort.java
+++ jdbc-demo/src/west_2/Sort.java
@@ -5,7 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class Sort {
     public static ArrayList<Product> sortProduct(String Item, String function) throws SQLException, ClassNotFoundException {
@@ -18,16 +17,18 @@ public class Sort {
                 PreparedStatement pstmt=conn.prepareStatement(sql);
                 try(ResultSet rs=pstmt.executeQuery()){
                     conn.setAutoCommit(false);
-                    int id=rs.getInt("Product_id");
-                    String name=rs.getString("Product_Name");
-                    int stock=rs.getInt("Product_Stock");
-                    double price=rs.getDouble("Product_price");
-                    Product item=new Product();
-                    item.setProduct_id(id+"");
-                    item.setProduct_name(name);
-                    item.setProduct_Stock(stock+"");
-                    item.setProduct_price(price+"");
-                    list.add(item);
+                    while (rs.next()) {
+                        int id = rs.getInt("Product_id");
+                        String name = rs.getString("Product_Name");
+                        int stock = rs.getInt("Product_Stock");
+                        double price = rs.getDouble("Product_price");
+                        Product item = new Product();
+                        item.setProduct_id(id + "");
+                        item.setProduct_name(name);
+                        item.setProduct_Stock(stock + "");
+                        item.setProduct_price(price + "");
+                        list.add(item);
+                    }
                 }
                 catch (Exception e){
                     conn.rollback();
@@ -49,20 +50,18 @@ public class Sort {
             if(Item.equals("Order_id")||Item.equals("Order_date")){
                 sql="select Order_id,Order_date,Order_price from order1 order by "+Item+" "+function;
                 PreparedStatement pstmt=conn.prepareStatement(sql);
-                System.out.println(sql);
                 try(ResultSet rs=pstmt.executeQuery()){
                     conn.setAutoCommit(false);
-
+                    while (rs.next()) {
                     int id=rs.getInt("Order_id");
                     String date=rs.getString("Order_date");
                     String price=rs.getString("Order_price");
-
                     Order item=new Order();
-
                     item.setOrder_id(id+"");
                     item.setOrder_date(date);
                     item.setOrder_price(price);
                     list.add(item);
+                }
                 } catch (Exception e){
                     conn.rollback();
                     throw new MyfunctionException();
